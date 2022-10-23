db92e781ff30291c1252e7853d6980b4bafeb000
AndrewFedorchenko <fedorchenko.andrey29071995@gmail.com>
2022-10-23 19:17:57 +0300

    finish_v1.0

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,21 +2,29 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
 const { ctrlWrapper } = require("../../helpers");
-const { validation } = require("../../middlewares");
-const schemas = require("../../schemas/contacts");
+const { validation, validationParams } = require("../../middlewares");
+const { schemas } = require("../../models/contacts/contact");
 
 router.get("/", ctrlWrapper(ctrl.listContacts));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getById));
+router.get("/:contactId", validationParams(schemas.validateId), ctrlWrapper(ctrl.getById));
 
 router.post("/", validation(schemas.addSchema), ctrlWrapper(ctrl.addContact));
 
 router.delete("/:contactId", ctrlWrapper(ctrl.removeContact));
 
 router.put(
-  "/:contactId",
-  validation(schemas.addSchema),
-  ctrlWrapper(ctrl.updateContact)
+  	"/:contactId",
+	validation(schemas.addSchema),
+  	validationParams(schemas.validateId),
+  	ctrlWrapper(ctrl.updateContact)
+);
+
+router.patch(
+  "/:contactId/favorite",
+  validationParams(schemas.validateId),
+  validation(schemas.updateFavorite),
+  ctrlWrapper(ctrl.updateStatusContact)
 );
 
 module.exports = router;
